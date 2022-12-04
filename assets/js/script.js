a4b0255c71c518d4dfc053231a858bc5cd57ac13
Hugo Delbegue <hugodelbegue@gmail.com>
2022-12-05 00:12:41 +0100

    add new theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 // switch theme
 function switchTheme() {
     const url = document.getElementById('switchThemeLink');
-    const switchBtn = document.querySelectorAll('#switchBtn > span').forEach((el) => {
+    document.querySelectorAll('#switchBtn > span').forEach((el) => {
         el.addEventListener('click', () => {
             if (el.id === 'btn1') {
-                url.setAttribute("href", `./assets/css/default_theme.css`);
+                url.setAttribute("href", `./assets/css/themes/default_theme.css`);
             } else if (el.id === 'btn2') {
-                url.setAttribute("href", `./assets/css/gray_theme.css`);
+                url.setAttribute("href", `./assets/css/themes/gray_theme.css`);
+            } else if (el.id === 'btn3') {
+                url.setAttribute("href", `./assets/css/themes/red_theme.css`);
             } else {
-                url.setAttribute("href", `./assets/css/red_theme.css`);
+                url.setAttribute("href", `./assets/css/themes/yellow_theme.css`);
             }
         })
     });
